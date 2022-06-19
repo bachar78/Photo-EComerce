1516ac8fa67b85b19a8d176daf9c604a043d31c3
Bachar Daowd <bachar.daowd@gmail.com>
2022-06-19 18:05:25 +0200

    Project first steps install strapi & NExtjs

---
+++ frontend/pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Photo EComerce</title>
+        <meta name='description' content='Created by Bachar Daowd' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+       <h1>Hello NExt</h1>
+      </main>
+    </div>
+  )
+}
