3e4209cd1f748659e7fa24cd275d6c282cd3665c
Bachar Daowd <bachar.daowd@gmail.com>
2022-06-20 17:31:23 +0200

    styling up the product details

--- frontend/pages/product/[slug].jsx
+++ frontend/pages/product/[slug].jsx
@@ -1,10 +1,13 @@
 import { useQuery } from 'urql'
 import { GET_PRODUCT_QUERY } from '../../lib/query'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
+import { DetailsStyles, ProductInfo, Quantity, Buy } from '../../styles/ProductDetails'
+import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai'
+
 export default function ProductDetails() {
   //Fetch Slug
-  const {query} = useRouter()
-  console.log()
+  const { query } = useRouter()
+
   //Fetch Graphql data
   const [results] = useQuery({
     query: GET_PRODUCT_QUERY,
@@ -15,21 +18,24 @@ export default function ProductDetails() {
   //Check for the data coming in
   if (fetching) return <p>Loading ....</p>
   if (error) return <p>Oh no... {error.message}</p>
-  console.log(data)
+  //extract our data
+  const { title, description, image, price } = data.products.data[0].attributes
+
   return (
-    <div>
-      <img src='' alt='' />
-      <div>
-        <h3>Title</h3>
-        <p>description</p>
-      </div>
-      <div>
-        <span>Quantity</span>
-        <button>Plus</button>
-        <p>0</p>
-        <button>Minus</button>
-      </div>
-      <button>Add to cart</button>
-    </div>
+    <DetailsStyles>
+      <img src={image.data.attributes.formats.medium.url} alt={title} />
+      <ProductInfo>
+        <h2>{title}</h2>
+        <h3>${price}</h3>
+        <p>{description}</p>
+        <Quantity>
+          <span>Quantity</span>
+          <button><AiFillMinusCircle/></button>
+          <p>0</p>
+          <button><AiFillPlusCircle/></button>
+        </Quantity>
+        <Buy>Add to cart</Buy>
+      </ProductInfo>
+    </DetailsStyles>
   )
 }
