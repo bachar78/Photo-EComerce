0e9624e357f984881520ce4fae5dd17ce6273314
Bachar Daowd <bachar.daowd@gmail.com>
2022-06-21 22:21:31 +0200

    layout animation

--- frontend/components/Cart.jsx
+++ frontend/components/Cart.jsx
@@ -42,46 +42,39 @@ function Cart() {
             <FaShoppingCart />
           </EmptyStyle>
         )}
-        <AnimatePresence>
-          <Cards variants={cards} initial='hidden' animate='show'>
-            {cartItems.length >= 1 &&
-              cartItems.map((item) => {
-                return (
-                  <Card key={item.slug} variants={card}>
-                    <img
-                      src={item.image.data.attributes.formats.thumbnail.url}
-                      alt={item.title}
-                    />
-                    <CardInfo>
-                      <h3>{item.title}</h3>
-                      <h3>${item.price}</h3>
-                      <Quantity>
-                        <span>Quantity</span>
-                        <button onClick={() => onRemove(item)}>
-                          <AiFillMinusCircle />
-                        </button>
-                        <p>{item.quantity}</p>
-                        <button onClick={() => onAdd(item, 1)}>
-                          <AiFillPlusCircle />
-                        </button>
-                      </Quantity>
-                    </CardInfo>
-                  </Card>
-                )
-              })}
-          </Cards>
-          {cartItems.length >= 1 && (
-            <Checkout
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0 }}
-              transition={{ delay: 0.2 }}
-            >
-              <h3>Subtotal: ${totalPrice}</h3>
-              <button>Purchase</button>
-            </Checkout>
-          )}
-        </AnimatePresence>
+        <Cards layout variants={cards} initial='hidden' animate='show'>
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => {
+              return (
+                <Card layout key={item.slug} variants={card}>
+                  <img
+                    src={item.image.data.attributes.formats.thumbnail.url}
+                    alt={item.title}
+                  />
+                  <CardInfo>
+                    <h3>{item.title}</h3>
+                    <h3>${item.price}</h3>
+                    <Quantity>
+                      <span>Quantity</span>
+                      <button onClick={() => onRemove(item)}>
+                        <AiFillMinusCircle />
+                      </button>
+                      <p>{item.quantity}</p>
+                      <button onClick={() => onAdd(item, 1)}>
+                        <AiFillPlusCircle />
+                      </button>
+                    </Quantity>
+                  </CardInfo>
+                </Card>
+              )
+            })}
+        </Cards>
+        {cartItems.length >= 1 && (
+          <Checkout layout>
+            <h3>Subtotal: ${totalPrice}</h3>
+            <button>Purchase</button>
+          </Checkout>
+        )}
       </CartStyle>
     </CartWrapper>
   )
