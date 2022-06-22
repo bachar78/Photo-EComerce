b3c5246dc701b723702272904864c3e8703346f2
Bachar Daowd <bachar.daowd@gmail.com>
2022-06-22 11:58:50 +0200

    Add Coupon

--- frontend/pages/api/stripe.jsx
+++ frontend/pages/api/stripe.jsx
@@ -11,17 +11,36 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         shipping_address_collection: {
-          allowed_countries: ['US', 'CA', 'NL', 'SR', 'LB', 'BL', 'GR', 'FR'],
+          allowed_countries: [
+            'US',
+            'CA',
+            'NL',
+            'BG',
+            'SR',
+            'LB',
+            'UA',
+            'GR',
+            'FR',
+          ],
         },
+        allow_promotion_codes: true,
+        shipping_options: [
+          { shipping_rate: 'shr_1LDPrFCT3IA57tfBulobql1N' },
+          { shipping_rate: 'shr_1LDQHrCT3IA57tfBe4i7jGt4' },
+        ],
         line_items: req.body.map((item) => {
           return {
             price_data: {
-              currency: 'usd',
+              currency: 'eur',
               product_data: {
                 name: item.title,
                 images: [item.image.data.attributes.formats.thumbnail.url],
               },
               unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
             },
             quantity: item.quantity,
           }
